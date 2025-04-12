aa0c1fae26ed019c634892d42197f0eeb8227cd3
RDK7159357 <155869647+RDK7159357@users.noreply.github.com>
2025-04-12 12:15:05 +0530

    1.2

--- DeepCopy.java
+++ DeepCopy.java
@@ -1,5 +1,6 @@
-// import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class DeepCopy {
     public static void main(String[] args) {
@@ -30,24 +31,38 @@ public class DeepCopy {
         // System.out.println(Arrays.toString(d));
 
 
-        int a[] = {7,27,813,46,25,19,629};
-        
-        for(int i=0;i<a.length;i++){
-            for(int j=i+1;j<a.length;j++){
-                if((a[i]%10) > (a[j]%10)){
-                    int temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-                if(a[i]%10==a[j]%10){
-                    if(a[i] > a[j]){
-                        int temp = a[i];
-                        a[i] = a[j];
-                        a[j] = temp;
-                    }
-                }
-            }
-        }
-        System.out.println(Arrays.toString(a));
+        Integer a[] = {7,27,813,46,25,19,629};
+        // Sorting based on units place
+        ArrayList<Integer> numlist = new ArrayList<>(Arrays.asList(a));
+
+        numlist.sort(Comparator.comparingInt(n->n%10));
+        System.out.println("Sorting based on units place "+numlist);
+
+        // Sorting based on tens place
+        numlist.sort(Comparator.comparingInt(n->(n/10)%10));
+        System.out.println("Sorting based on tens place "+numlist);
+
+
+
+
+
+
+        // for(int i=0;i<a.length;i++){
+        //     for(int j=i+1;j<a.length;j++){
+        //         if((a[i]%10) > (a[j]%10)){
+        //             int temp = a[i];
+        //             a[i] = a[j];
+        //             a[j] = temp;
+        //         }
+        //         if(a[i]%10==a[j]%10){
+        //             if(a[i] > a[j]){
+        //                 int temp = a[i];
+        //                 a[i] = a[j];
+        //                 a[j] = temp;
+        //             }
+        //         }
+        //     }
+        // }
+        // System.out.println(Arrays.toString(a));
     }
 }
